1fb626b26e0a28eb9dbd5bf5df4a94e7d8b44580
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2004-02-01 15:15:03 +0000

    Build changes for Linux and version control patches
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@40 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Builder/builder_custom_init.sh
+++ Builder/builder_custom_init.sh
@@ -1,11 +1,13 @@
 #!/bin/echo Usage: . $0
 export WORKDIR=$(cd ..;pwd)
 
+export PY_MAJ=2
+export PY_MIN=3
 export SVNVER=0.37.0
 export PYCXX=${WORKDIR}/Import/pycxx_5_2_1
-export PYTHON=/usr/bin/python2.3
-export PYDIR=python2.3
-export PYLIB=python23
+export PYTHON=/usr/bin/python${PY_MAJ}.${PY_MIN}
+export PYDIR=python${PY_MAJ}.${PY_MIN}
+export PYLIB=python${PY_MAJ}${PY_MIN}
 export SVN_INC=/usr/include/subversion-1
 export SVN_LIB=/usr/lib
 export APR_INC=/usr/include/apr-0
@@ -13,5 +15,5 @@ export APR_LIB=/usr/lib
 export SVNCPP_INC=${HOME}/subversion/Linux_rh90/rapidsvn/include
 export SVNCPP_LIB=${HOME}/subversion/Linux_rh90/rapidsvn/src/svncpp/.libs
 
-# prove the python version selected
+# prove the python version selected is as expected
 ${PYTHON} -c "import sys;print 'Info: Python Version',sys.version"
---
+++ Patches/readme.txt
@@ -0,0 +1,6 @@
+This directory contains any patches that need to be applied to 3rd party code
+to allow pysvn to build.
+
+svncpp.patch
+	- patch against r7002
+	- add HTTPS support
