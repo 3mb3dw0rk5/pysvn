58434b99749bff7fa1bfe6d4c6cbc5bc4927c7c7
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2004-04-03 22:46:50 +0000

    Update build setup
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@101 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Builder/builder_custom_init.sh
+++ Builder/builder_custom_init.sh
@@ -4,7 +4,6 @@ export WORKDIR=$(cd ..;pwd)
 # default to 2.3 if no value in $1 and $2
 export PY_MAJ=${1:-2}
 export PY_MIN=${2:-3}
-export SVNVER=1.0.0
 export PYCXX=${WORKDIR}/Import/pycxx_5_2_1
 export PYTHON=/usr/bin/python${PY_MAJ}.${PY_MIN}
 export PYDIR=python${PY_MAJ}.${PY_MIN}
@@ -15,8 +14,8 @@ export APR_INC=/usr/include/apr-0
 export APR_LIB=/usr/lib
 
 # always build rapidsvn into an unversioned dir
-export SVNCPP_INC=${HOME}/subversion/Linux_rh90/rapidsvn/include
-export SVNCPP_LIB=${HOME}/subversion/Linux_rh90/rapidsvn/src/svncpp/.libs
+export SVNCPP_INC=${TARGET}/rapidsvn/include
+export SVNCPP_LIB=${TARGET}/rapidsvn/src/svncpp/.libs
 export LD_LIBRARY_PATH=${SVNCPP_LIB}
 
 # prove the python version selected is as expected
