bac039baf37e22b4784c7fccfa0306897623f24e
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2005-01-20 23:58:19 +0000

    Python 2.2 does not have the yield builtin
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@307 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Tests/find.py
+++ Tests/find.py
@@ -7,46 +7,17 @@
 import os
 import sys
 
-# copied from python 2.3 as we need to run on 2.2 that does not have walk
-def walk(top, topdown=True, onerror=None):
-    from os.path import join, isdir, islink
-
-    # We may not have read permission for top, in which case we can't
-    # get a list of the files the directory contains.  os.path.walk
-    # always suppressed the exception then, rather than blow up for a
-    # minor reason when (say) a thousand readable directories are still
-    # left to visit.  That logic is copied here.
-    try:
-        # Note that listdir and error are globals in this module due
-        # to earlier import-*.
-        names = os.listdir(top)
-    except error, err:
-        if onerror is not None:
-            onerror(err)
-        return
-
-    dirs, nondirs = [], []
-    for name in names:
-        if isdir(join(top, name)):
-            dirs.append(name)
-        else:
-            nondirs.append(name)
-
-    if topdown:
-        yield top, dirs, nondirs
-    for name in dirs:
-        path = join(top, name)
-        if not islink(path):
-            for x in walk(path, topdown, onerror):
-                yield x
-    if not topdown:
-        yield top, dirs, nondirs
-
-
 all_files = []
-for root, dirs, files in walk( sys.argv[1] ):
-    for name in files:
-        all_files.append( os.path.join( root, name ) )
+
+def walk( dirname, all_files ):
+    for filename in os.listdir( dirname ):
+        filename = os.path.join( dirname, filename )
+        if os.path.isdir( filename ):    
+            walk( filename, all_files )
+        else:
+            all_files.append( filename )
+
+walk( sys.argv[1], all_files )
 
 all_files.sort()
 for file in all_files:
