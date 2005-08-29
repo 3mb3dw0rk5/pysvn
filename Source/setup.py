d854a0c9b20de97bb0dac3d13d5266480136ea6e
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2005-08-29 14:07:42 +0000

    Cannot use the libintl.a on fink it keeps on changing from the .1 to the .3 and back making it impossible to reproduce a success  link after updating fink.
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@437 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Source/setup.py
+++ Source/setup.py
@@ -17,10 +17,11 @@ import os
 import distutils
 import distutils.sysconfig
 
+import xml.dom.minidom
+import xml.sax
+
 class SetupError(Exception):
     pass
-
-is_darwin = sys.platform == 'darwin'
 
 def main( argv ):
     if sys.platform == 'win32':
@@ -29,7 +30,8 @@ def main( argv ):
 
     try:
         if argv[1:2] == ['configure']:
-            return create_makefile( argv )
+            creater = MakeFileCreater()
+            return creater.createMakefile( argv )
         elif argv[1:2] == ['help']:
             return help( argv )
         else:
@@ -57,49 +59,84 @@ def help( argv ):
 ''' % (basename, basename)
     return 1
 
-def create_makefile( argv ):
+class MakeFileCreater:
+    def __init__( self ):
+        self.is_mac_os_x = False
+        self.mac_os_x_version = None
+        if os.path.exists( '/System/Library/CoreServices/SystemVersion.plist' ):
+            dom = xml.dom.minidom.parse( file( '/System/Library/CoreServices/SystemVersion.plist' ) )
+            plist = dom.getElementsByTagName( 'plist' )[0]
+            plist_dict = plist.getElementsByTagName( 'dict' )[0]
+            for key_or_value in plist_dict.childNodes:
+                if key_or_value.nodeName == 'key' and self.node_text( key_or_value.childNodes ) == 'ProductVersion':
+                    value_node = key_or_value.nextSibling
+                    while value_node.nodeType == xml.dom.minidom.Node.TEXT_NODE:
+                        value_node = value_node.nextSibling
 
-    include_dir_list = []
+                    self.mac_os_x_version_string = self.node_text( value_node.childNodes )
+                    print 'Info: Mac OS X Version',self.mac_os_x_version_string
+                    self.mac_os_x_version = [int(s) for s in self.mac_os_x_version_string.split('.')]
+                    self.is_mac_os_x = True
 
-    # add python include dirs
-    include_dir_list.append( distutils.sysconfig.get_python_inc() )
-    if distutils.sysconfig.get_python_inc() != distutils.sysconfig.get_python_inc( True ):
-        include_dir_list.append( distutils.sysconfig.get_python_inc( True ) )
+    def node_text( self, all_nodes ):
+        all_text = []
+        for node in all_nodes:
+            if node.nodeType == xml.dom.minidom.Node.TEXT_NODE:
+                all_text.append( node.data )
+        return ''.join( all_text )
 
-    # add pycxx include
-    pycxx_dir = find_pycxx( argv )
-    include_dir_list.append( pycxx_dir )
-    # add SVN include
-    include_dir_list.append( find_svn_inc( argv ) )
-    # add APR include
-    include_dir_list.append( find_apr_inc( argv ) )
+    def cmp_mac_os_x_version( self, version ):
+        return cmp( self.mac_os_x_version[0:len(version)], list(version) )
 
-    # add source dir
-    include_dir_list.append( '.' )
+    def createMakefile( self, argv ):
+        include_dir_list = []
 
-    template_values = {
-        # python executable
-        'python_exe':       sys.executable,
+        # add python include dirs
+        include_dir_list.append( distutils.sysconfig.get_python_inc() )
+        if distutils.sysconfig.get_python_inc() != distutils.sysconfig.get_python_inc( True ):
+            include_dir_list.append( distutils.sysconfig.get_python_inc( True ) )
 
-        # includes
-        'includes':         ' '.join( ['-I%s' % include_dir for include_dir in include_dir_list] ),
+        # add pycxx include
+        pycxx_dir = self.find_pycxx( argv )
+        include_dir_list.append( pycxx_dir )
+        # add SVN include
+        include_dir_list.append( self.find_svn_inc( argv ) )
+        # add APR include
+        include_dir_list.append( self.find_apr_inc( argv ) )
 
-        # add svn lib dir
-        'svn_lib_dir':      find_svn_lib( argv ),
+        # add source dir
+        include_dir_list.append( '.' )
 
-        # pycxx src dir
-        'pycxx_dir':        pycxx_dir
-        }
+        template_values = {
+            # python executable
+            'python_exe':       sys.executable,
 
-    print 'Info: Creating Makefile'
-    makefile = file( 'Makefile', 'w' )
-    if sys.platform == 'darwin':
-        makefile.write( makefile_template_macosx % template_values )
-    else:
-        makefile.write( makefile_template % template_values )
-    makefile.close()
+            # includes
+            'includes':         ' '.join( ['-I%s' % include_dir for include_dir in include_dir_list] ),
 
-makefile_template = '''#
+            # add svn lib dir
+            'svn_lib_dir':      self.find_svn_lib( argv ),
+
+            # pycxx src dir
+            'pycxx_dir':        pycxx_dir
+            }
+
+        print 'Info: Creating Makefile'
+        makefile = file( 'Makefile', 'w' )
+        if self.is_mac_os_x:
+            if self.cmp_mac_os_x_version( (10,4) ) >= 0:
+                print 'Info: Using Mac OS X 10.4 makefile template'
+                template_values['frameworks'] = '-framework System -framework Python -framework CoreFoundation -framework Kerberos'
+            else:
+                print 'Info: Using Mac OS X 10.3 makefile template'
+                template_values['frameworks'] = '-framework System -framework Python -framework CoreFoundation'
+            makefile.write( self.makefile_template_macosx % template_values )
+        else:
+            print 'Info: Using unix makefile template'
+            makefile.write( self.makefile_template % template_values )
+        makefile.close()
+
+    makefile_template = '''#
 #	Created by pysvn Extension/Source/setup.py
 #
 PYTHON=%(python_exe)s
@@ -117,7 +154,7 @@ include pysvn_common.mak
 '''
 
 
-makefile_template_macosx = '''#
+    makefile_template_macosx = '''#
 #	Created by pysvn Extension/Source/setup.py
 #
 PYTHON=%(python_exe)s
@@ -126,7 +163,7 @@ CCCFLAGS=-Wall -Wno-long-double -fPIC -fexceptions -frtti %(includes)s
 CC=gcc -c
 CCFLAGS=-Wall -Wno-long-double -fPIC %(includes)s
 PYCXX=%(pycxx_dir)s
-LDSHARED=g++ -bundle -g -u _PyMac_Error -framework System -framework Python -framework CoreFoundation
+LDSHARED=g++ -bundle -g -u _PyMac_Error %(frameworks)s
 LDLIBS= \
 %(svn_lib_dir)s/libsvn_client-1.a \
 %(svn_lib_dir)s/libsvn_subr-1.a \
@@ -149,79 +186,79 @@ LDLIBS= \
 %(svn_lib_dir)s/libexpat.a \
 %(svn_lib_dir)s/libxml2.a \
 %(svn_lib_dir)s/libdb-4.3.a \
-%(svn_lib_dir)s/libintl.a \
-\
- -liconv -lz
+-L%(svn_lib_dir)s\
+ -lintl.3 -liconv.2 -lz
 include pysvn_common.mak
 '''
 
-def find_pycxx( argv ):
-    return find_dir( argv,
-                'PyCXX include',
-                '--pycxx-dir=',
-                [   '../Import/pycxx_5_3_3'],
-                'CXX/Version.hxx' )
+    def find_pycxx( self, argv ):
+        return self.find_dir( argv,
+                    'PyCXX include',
+                    '--pycxx-dir=',
+                    [   '../Import/pycxx_5_3_4',
+                        '../Import/pycxx_5_3_3'],
+                    'CXX/Version.hxx' )
 
-def find_svn_inc( argv ):
-    return find_dir( argv,
-                'SVN include',
-                '--svn-inc-dir=',
-                [   '/usr/include/subversion-1',            # typical Linux
-                    '/usr/local/include/subversion-1',      # typical *BSD
-                    '/sw/include/subversion-1'],            # Darwin
-                'svn_client.h' )
+    def find_svn_inc( self, argv ):
+        return self.find_dir( argv,
+                    'SVN include',
+                    '--svn-inc-dir=',
+                    [   '/usr/include/subversion-1',            # typical Linux
+                        '/usr/local/include/subversion-1',      # typical *BSD
+                        '/sw/include/subversion-1'],            # Darwin
+                    'svn_client.h' )
 
-def find_svn_lib( argv ):
-    return find_dir( argv,
-                'SVN library',
-                '--svn-lib-dir=',
-                [   '/usr/lib',                             # typical Linux
-                    '/usr/local/lib',                       # typical *BSD
-                    '/sw/lib'],                             # Darwin
-                is_darwin and 'libsvn_client-1.dylib' or 'libsvn_client-1.so' )
+    def find_svn_lib( self, argv ):
+        return self.find_dir( argv,
+                    'SVN library',
+                    '--svn-lib-dir=',
+                    [   '/usr/lib',                             # typical Linux
+                        '/usr/local/lib',                       # typical *BSD
+                        '/sw/lib'],                             # Darwin
+                    self.is_mac_os_x and 'libsvn_client-1.dylib' or 'libsvn_client-1.so' )
 
-def find_apr_inc( argv ):
-    return find_dir( argv,
-                'APR include',
-                '--apr-inc-dir=',
-                [   '/usr/include/apr-0',                   # typical Linux
-                    '/usr/include/apache2',                 # alternate Linux
-                    '/usr/local/include/apr0',              # typical *BSD
-                    '/usr/local/include/apache2',           # alternate *BSD
-                    '/sw/include/apr-0'],                   # Darwin
-                'apr.h' )
+    def find_apr_inc( self, argv ):
+        return self.find_dir( argv,
+                    'APR include',
+                    '--apr-inc-dir=',
+                    [   '/usr/include/apr-0',                   # typical Linux
+                        '/usr/include/apache2',                 # alternate Linux
+                        '/usr/local/include/apr0',              # typical *BSD
+                        '/usr/local/include/apache2',           # alternate *BSD
+                        '/sw/include/apr-0'],                   # Darwin
+                    'apr.h' )
 
-def find_apr_lib( argv ):
-    return find_dir( argv,
-                'APR library',
-                '--apr-lib-dir=',
-                [   '/usr/lib',                             # typical Linux
-                    '/usr/local/lib',                       # typical *BSD
-                    '/sw/lib'],                             # Darwin
-                is_darwin and 'libapr-0.dylib' or 'libapr-0.so' )
+    def find_apr_lib( self, argv ):
+        return self.find_dir( argv,
+                    'APR library',
+                    '--apr-lib-dir=',
+                    [   '/usr/lib',                             # typical Linux
+                        '/usr/local/lib',                       # typical *BSD
+                        '/sw/lib'],                             # Darwin
+                    self.is_mac_os_x and 'libapr-0.dylib' or 'libapr-0.so' )
 
-def find_dir( argv, name, kw, base_dir_list, check_file=None ):
-    dir = __find_dir( argv, name, kw, base_dir_list, check_file )
-    print 'Info: Found',name,'in',dir
-    return dir
+    def find_dir( self, argv, name, kw, base_dir_list, check_file=None ):
+        dir = self.__find_dir( argv, name, kw, base_dir_list, check_file )
+        print 'Info: Found',name,'in',dir
+        return dir
 
-def __find_dir( argv, name, kw, base_dir_list, check_file=None ):
-    for arg in argv[2:]:
-        if arg[0:len(kw)] == kw:
-            return arg[len(kw):]
+    def __find_dir( self, argv, name, kw, base_dir_list, check_file=None ):
+        for arg in argv[2:]:
+            if arg[0:len(kw)] == kw:
+                return arg[len(kw):]
 
-    # expect to find the pycxx in this source tree
-    for dir in base_dir_list:
-        print 'Info: Looking for',name,'in',dir
-        if check_file is not None:
-            full_check_file = os.path.join( dir, check_file )
-            print 'Info: Checking for',full_check_file
-            if os.path.exists( full_check_file ):
+        # expect to find the pycxx in this source tree
+        for dir in base_dir_list:
+            print 'Info: Looking for',name,'in',dir
+            if check_file is not None:
+                full_check_file = os.path.join( dir, check_file )
+                print 'Info: Checking for',full_check_file
+                if os.path.exists( full_check_file ):
+                    return os.path.abspath( dir )
+            elif os.path.exists( dir ):
                 return os.path.abspath( dir )
-        elif os.path.exists( dir ):
-            return os.path.abspath( dir )
 
-    raise SetupError, 'cannot find %s - use %s' % (name, kw)
+        raise SetupError, 'cannot find %s - use %s' % (name, kw)
 
 
 if __name__ == '__main__':
