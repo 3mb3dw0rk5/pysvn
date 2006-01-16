3c98da2155abcd1758ce4225b7e43ce8f41a6aa0
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2006-01-16 21:38:33 +0000

    Support kitting against the darwin ports
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@534 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Kit/MacOSX/make_pkg.py
+++ Kit/MacOSX/make_pkg.py
@@ -1,5 +1,5 @@
 #
-#	make_pkg.py
+#    make_pkg.py
 #
 import os
 
@@ -24,66 +24,77 @@ year = time.strftime( '%Y', time.localtime( build_time ) )
 tmpdir = os.path.join( os.getcwd(), 'tmp' )
 
 if os.path.exists( tmpdir ):
-	print 'Info: Clean up tmp directory'
-	os.system( 'rm -rf tmp' )
+    print 'Info: Clean up tmp directory'
+    os.system( 'rm -rf tmp' )
 
 print 'Info: Create directories'
 
 for kit_dir in [
-	tmpdir,
-	os.path.join( tmpdir, 'Resources' ),
-	os.path.join( tmpdir, 'Contents' ),
-	os.path.join( tmpdir, 'Contents/pysvn' ),
-	os.path.join( tmpdir, '%s' % pkg_filename),
-	os.path.join( tmpdir, '%s/Examples' % pkg_filename ),
-	os.path.join( tmpdir, '%s/Examples/Client' % pkg_filename ),
-	os.path.join( tmpdir, '%s/Documentation' % pkg_filename),
-	]:
-	if not os.path.exists( kit_dir ):
-		os.makedirs( kit_dir )
+    tmpdir,
+    os.path.join( tmpdir, 'Resources' ),
+    os.path.join( tmpdir, 'Contents' ),
+    os.path.join( tmpdir, 'Contents/pysvn' ),
+    os.path.join( tmpdir, '%s' % pkg_filename),
+    os.path.join( tmpdir, '%s/Examples' % pkg_filename ),
+    os.path.join( tmpdir, '%s/Examples/Client' % pkg_filename ),
+    os.path.join( tmpdir, '%s/Documentation' % pkg_filename),
+    ]:
+    if not os.path.exists( kit_dir ):
+        os.makedirs( kit_dir )
 
 
+print 'Info: Finding dylibs used by pysvn'
+dylib_list = []
+os.system( 'otool -L ../../Source/pysvn/_pysvn.so >/tmp/pysvn_otool.tmp' )
+for line in file( '/tmp/pysvn_otool.tmp' ).readlines():
+    line = line.strip()
+    if( line.startswith( '/' )
+    and not line.startswith( '/usr' )
+    and not line.startswith( '/System' ) ):
+        libpath = line.split()[0]
+        print 'Info: Need lib',libpath
+        dylib_list.append( libpath )
+        
 print 'Info: Copy files'
-for cp_src, cp_dst_dir_fmt in [
-	('../../Source/pysvn/__init__.py',
-		'Contents/pysvn'),
-	('../../Source/pysvn/_pysvn.so',
-		'Contents/pysvn'),
-	('/sw/lib/libintl.3.dylib',
-		'Contents/pysvn'),
-	('/sw/lib/libiconv.2.dylib',
-		'Contents/pysvn'),
-	('../../LICENSE.txt',
-		'Resources/License.txt'),
-	('../../LICENSE.txt',
-		'%s/License.txt' % pkg_filename ),
-	('../../Docs/pysvn.html',
-		'%s/Documentation' % pkg_filename ),
-	('../../Docs/pysvn_prog_ref.html',
-		'%s/Documentation' % pkg_filename ),
-	('../../Docs/pysvn_prog_guide.html',
-		'%s/Documentation' % pkg_filename ),
-	('../../Examples/Client/svn_cmd.py',
-		'%s/Examples/Client' % pkg_filename ),
-	('../../Examples/Client/parse_datetime.py',
-		'%s/Examples/Client' % pkg_filename ),
-	]:
-	print 'Info:  cp %s' % cp_src
-	os.system( 'cp -f %s tmp/%s' % (cp_src, cp_dst_dir_fmt % locals()) )
+
+cp_list = [
+    ('../../Source/pysvn/__init__.py',
+        'Contents/pysvn'),
+    ('../../Source/pysvn/_pysvn.so',
+        'Contents/pysvn'),
+    ('../../LICENSE.txt',
+        'Resources/License.txt'),
+    ('../../LICENSE.txt',
+        '%s/License.txt' % pkg_filename ),
+    ('../../Docs/pysvn.html',
+        '%s/Documentation' % pkg_filename ),
+    ('../../Docs/pysvn_prog_ref.html',
+        '%s/Documentation' % pkg_filename ),
+    ('../../Docs/pysvn_prog_ref.js',
+        '%s/Documentation' % pkg_filename ),
+    ('../../Docs/pysvn_prog_guide.html',
+        '%s/Documentation' % pkg_filename ),
+    ('../../Examples/Client/svn_cmd.py',
+        '%s/Examples/Client' % pkg_filename ),
+    ('../../Examples/Client/parse_datetime.py',
+        '%s/Examples/Client' % pkg_filename ),
+    ]
+
+for libpath in dylib_list:
+    cp_list.append( (libpath, 'Contents/pysvn') )
+
+for cp_src, cp_dst_dir_fmt in cp_list:
+    print 'Info:  cp %s' % cp_src
+    os.system( 'cp -f %s tmp/%s' % (cp_src, cp_dst_dir_fmt % locals()) )
 
 print 'Info: Fix the install paths for the dylib files'
-os.system( 'install_name_tool'
-    ' -change'
-    ' /sw/lib/libintl.3.dylib'
-    ' /System/Library/Frameworks/Python.framework/Versions/2.3/lib/python2.3/site-packages/pysvn/libintl.3.dylib'
-    ' %s' %
-    'tmp/Contents/pysvn/_pysvn.so' )
-os.system( 'install_name_tool'
-    ' -change'
-    ' /sw/lib/libiconv.2.dylib'
-    ' /System/Library/Frameworks/Python.framework/Versions/2.3/lib/python2.3/site-packages/pysvn/libiconv.2.dylib'
-    ' %s' %
-    'tmp/Contents/pysvn/_pysvn.so' )
+for libpath in dylib_list:
+    os.system( 'install_name_tool'
+        ' -change'
+        ' %s'
+        ' /System/Library/Frameworks/Python.framework/Versions/2.3/lib/python2.3/site-packages/pysvn/%s'
+        ' %s' %
+            (libpath, os.path.basename( libpath ), 'tmp/Contents/pysvn/_pysvn.so') )
 
 print 'Info: Create tmp/Resources/ReadMe.txt'
 f = file('tmp/Resources/ReadMe.txt','w')
@@ -109,54 +120,54 @@ f.write('''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-	<key>CFBundleGetInfoString</key>
-	<string>pysvn Extension %(pysvn_version_string)s</string>
-	<key>CFBundleIdentifier</key>
-	<string>org.tigris.pysvn.extension</string>
-	<key>CFBundleName</key>
-	<string>pysvn Extension</string>
-	<key>CFBundleShortVersionString</key>
-	<string>%(pysvn_version_string)s</string>
-	<key>IFMajorVersion</key>
-	<integer>%(pysvnmaj)s</integer>
-	<key>IFMinorVersion</key>
-	<integer>%(pysvnmin)s</integer>
-	<key>IFPkgFlagAllowBackRev</key>
-	<false/>
-	<key>IFPkgFlagAuthorizationAction</key>
-	<string>AdminAuthorization</string>
-	<key>IFPkgFlagDefaultLocation</key>
-	<string>/System/Library/Frameworks/Python.framework/Versions/%(pymaj)s.%(pymin)s/lib/python%(pymaj)s.%(pymin)s/site-packages</string>
-	<key>IFPkgFlagInstallFat</key>
-	<false/>
-	<key>IFPkgFlagIsRequired</key>
-	<false/>
-	<key>IFPkgFlagRelocatable</key>
-	<false/>
-	<key>IFPkgFlagRestartAction</key>
-	<string>NoRestart</string>
-	<key>IFPkgFlagRootVolumeOnly</key>
-	<true/>
-	<key>IFPkgFlagUpdateInstalledLanguages</key>
-	<false/>
-	<key>IFPkgFormatVersion</key>
-	<real>0.10000000149011612</real>
+    <key>CFBundleGetInfoString</key>
+    <string>pysvn Extension %(pysvn_version_string)s</string>
+    <key>CFBundleIdentifier</key>
+    <string>org.tigris.pysvn.extension</string>
+    <key>CFBundleName</key>
+    <string>pysvn Extension</string>
+    <key>CFBundleShortVersionString</key>
+    <string>%(pysvn_version_string)s</string>
+    <key>IFMajorVersion</key>
+    <integer>%(pysvnmaj)s</integer>
+    <key>IFMinorVersion</key>
+    <integer>%(pysvnmin)s</integer>
+    <key>IFPkgFlagAllowBackRev</key>
+    <false/>
+    <key>IFPkgFlagAuthorizationAction</key>
+    <string>AdminAuthorization</string>
+    <key>IFPkgFlagDefaultLocation</key>
+    <string>/System/Library/Frameworks/Python.framework/Versions/%(pymaj)s.%(pymin)s/lib/python%(pymaj)s.%(pymin)s/site-packages</string>
+    <key>IFPkgFlagInstallFat</key>
+    <false/>
+    <key>IFPkgFlagIsRequired</key>
+    <false/>
+    <key>IFPkgFlagRelocatable</key>
+    <false/>
+    <key>IFPkgFlagRestartAction</key>
+    <string>NoRestart</string>
+    <key>IFPkgFlagRootVolumeOnly</key>
+    <true/>
+    <key>IFPkgFlagUpdateInstalledLanguages</key>
+    <false/>
+    <key>IFPkgFormatVersion</key>
+    <real>0.10000000149011612</real>
 </dict>
 </plist>
 ''' % locals() )
 f.close()
 
 print 'Info: PackageMaker'
-cmd = [	'/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker',
-	'-build',
-	'-p %s' % os.path.abspath( 'tmp/%s/%s.pkg' % (pkg_filename, pkg_filename) ),
-	'-f %s' % os.path.abspath( 'tmp/Contents' ),
-	'-r %s' % os.path.abspath( 'tmp/Resources' ),
-	'-i %s' % os.path.abspath( 'tmp/Info.plist' ),
-	]
+cmd = [    '/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker',
+    '-build',
+    '-p %s' % os.path.abspath( 'tmp/%s/%s.pkg' % (pkg_filename, pkg_filename) ),
+    '-f %s' % os.path.abspath( 'tmp/Contents' ),
+    '-r %s' % os.path.abspath( 'tmp/Resources' ),
+    '-i %s' % os.path.abspath( 'tmp/Info.plist' ),
+    ]
 os.system( ' '.join( cmd ) )
 
 print 'Info: Make Disk Image'
 os.system( 'hdiutil create -srcfolder tmp/%s tmp/tmp.dmg' % pkg_filename )
 os.system( 'hdiutil convert tmp/tmp.dmg -format UDZO -imagekey zlib-level=9 ' 
-		'-o tmp/%s.dmg' % pkg_filename )
+        '-o tmp/%s.dmg' % pkg_filename )
