5de0d20ebc3ba8369c761ec00f1cd3ff76e449de
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2006-09-28 11:25:52 +0000

    need to add more libs to the link on linux for 1.4.0 to support gss
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@709 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Source/setup.py
+++ Source/setup.py
@@ -258,7 +258,8 @@ LDSHARED=g++ -shared %(debug_cflags)s
 LDLIBS=-L%(svn_lib_dir)s \
 -lsvn_client-1 \
 -lsvn_diff-1 \
--lsvn_repos-1
+-lsvn_repos-1 \
+ -lgssapi_krb5 -lkrb5 -lk5crypto -lkrb5support -lcom_err -lresolv -lexpat
 
 #include pysvn_common.mak
 '''
