8c8e8c3f0941c33206dbcf1c1182d9ba68c38ec9
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2009-04-04 13:15:39 +0000

    support 1.6
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@1155 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Source/run_vcexpress.cmd
+++ Source/run_vcexpress.cmd
@@ -1,2 +1,3 @@
 if "%SVN_VER_MAJ_MIN%" == "1.4" vcexpress pysvn-for-svn-1-4.sln /useenv
 if "%SVN_VER_MAJ_MIN%" == "1.5" vcexpress pysvn-for-svn-1-5.sln /useenv
+if "%SVN_VER_MAJ_MIN%" == "1.6" vcexpress pysvn-for-svn-1-6.sln /useenv
