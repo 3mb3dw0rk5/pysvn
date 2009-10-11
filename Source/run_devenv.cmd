199b2b185c78cd1a84a4f72b07c0f384022dce94
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2009-10-11 15:19:12 +0000

    Add MS VC++ 9.0 Aka VCExpress 2008
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@1263 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Source/run_devenv.cmd
+++ Source/run_devenv.cmd
@@ -1,3 +1,3 @@
-if "%SVN_VER_MAJ_MIN%" == "1.4" devenv pysvn-for-svn-1-4.sln /useenv
-if "%SVN_VER_MAJ_MIN%" == "1.5" devenv pysvn-for-svn-1-5.sln /useenv
-if "%SVN_VER_MAJ_MIN%" == "1.6" devenv pysvn-for-svn-1-6.sln /useenv
+if "%SVN_VER_MAJ_MIN%" == "1.4" devenv pysvn-for-svn-1-4-msvc71.sln /useenv
+if "%SVN_VER_MAJ_MIN%" == "1.5" devenv pysvn-for-svn-1-5-msvc71.sln /useenv
+if "%SVN_VER_MAJ_MIN%" == "1.6" devenv pysvn-for-svn-1-6-msvc71.sln /useenv
