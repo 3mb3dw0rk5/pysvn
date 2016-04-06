27d10e662bef3cc6fabea35b422378b8e73f8cdb
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2016-04-06 18:23:47 +0000

    put all the build smarts into build.cmd and leave builder_custom_init.cmd empty for now this ensures that the path is the short and does not contain extra compilers
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@1736 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Source/build.cmd
+++ Source/build.cmd
@@ -1,10 +1,42 @@
-set PY_VER=%1
-set SVN_VER_MAJ_MIN=%2
-set BUILD_ARCH=%3
+if not "%1" == "" set PY_VER=%1
+if not "%2" == "" set SVN_VER_MAJ_MIN=%2
+if not "%3" == "" set BUILD_ARCH=%3
 
-call ..\Builder\builder_custom_init.cmd
+if "%PY_VER%" == "" goto :eof
+if "%SVN_VER_MAJ_MIN%" == "" goto :eof
+if "%BUILD_ARCH%" == "" goto :eof
 
-python setup.py configure --verbose --platform=%BUILD_ARCH% --pycxx-dir=%USERPROFILE%\wc\svn\PyCxx --distro-dir=%TARGET%\dist
+if /i "%BUILD_ARCH%" EQU "win32" set BUILD_ARCH=Win32
+if /i "%BUILD_ARCH%" EQU "win64" set BUILD_ARCH=Win64
+
+set SVN_VER_MAJ_DASH_MIN=%SVN_VER_MAJ_MIN:.=-%
+set PY_MAJ=%PY_VER:~0,1%
+set PY_MIN=%PY_VER:~2%
+
+rem Save CWD
+pushd .
+
+pushd ..\..\ReleaseEngineering\Windows
+call build-config.cmd %PY_VER% %SVN_VER_MAJ_MIN% %BUILD_ARCH%
+popd
+
+set PYCXX=%BUILDER_TOP_DIR%\Import\pycxx-%PYCXX_VER%
+set PY=c:\python%PY_MAJ%%PY_MIN%.%BUILD_ARCH%
+if not exist %PY%\python.exe set PY=c:\python%PY_MAJ%%PY_MIN%
+
+set PYTHONPATH=%BUILDER_TOP_DIR%\Source
+
+rem prove the python version selected works
+%PYTHON% -c "import sys;print( 'Info: Python Version %%s' %% sys.version )"
+echo SVN Version:
+svn --version --quiet
+
+rem restore original CWD
+popd
+
+%PYTHON% setup.py configure --verbose --platform=%BUILD_ARCH% --pycxx-dir=%USERPROFILE%\wc\svn\PyCxx --distro-dir=%TARGET%\dist
+
+del pysvn\_pysvn*
 
 if ERRORLEVEL 1 goto :EOF
 nmake clean
