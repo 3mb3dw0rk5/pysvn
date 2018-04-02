aea331398ad94ff640704a8b534ba6a074539cd1
barryscott <barryscott@8a6430f3-a0d1-4a6a-aeff-73e05446c159>
2018-04-02 18:59:44 +0000

    Look for anaconda python as well
    git-svn-id: https://svn.code.sf.net/p/pysvn/code/trunk/pysvn/Extension@1947 8a6430f3-a0d1-4a6a-aeff-73e05446c159

--- Kit/Windows/show_python_registry.py
+++ Kit/Windows/show_python_registry.py
@@ -7,25 +7,26 @@ def main( argv ):
         for level, key in [('HKLM', winreg.HKEY_LOCAL_MACHINE), ('HKCU', winreg.HKEY_CURRENT_USER)]:
             # look for python in the registry
             try:
-                core_key = winreg.OpenKey( key, r'SOFTWARE\Python\PythonCore', 0, winreg.KEY_READ | view )
-                try:
-                    index = 0
-                    while True:
-                        py_name = winreg.EnumKey( core_key, index )
-                        py_key = winreg.OpenKey( core_key, py_name, 0, winreg.KEY_READ | view )
-                        install_key = winreg.OpenKey( py_key, 'InstallPath', 0, winreg.KEY_READ | view )
-                        install_path, value_type = winreg.QueryValueEx( install_key, None )
-                        print( arch, py_name, install_path )
+                for vendor_key in (r'SOFTWARE\Python\PythonCore', r'SOFTWARE\Python\ContinuumAnalytics'):
+                    core_key = winreg.OpenKey( key, vendor_key, 0, winreg.KEY_READ | view )
+                    try:
+                        index = 0
+                        while True:
+                            py_name = winreg.EnumKey( core_key, index )
+                            py_key = winreg.OpenKey( core_key, py_name, 0, winreg.KEY_READ | view )
+                            install_key = winreg.OpenKey( py_key, 'InstallPath', 0, winreg.KEY_READ | view )
+                            install_path, value_type = winreg.QueryValueEx( install_key, None )
+                            print( arch, py_name, install_path )
 
-                        index += 1
+                            index += 1
 
-                except WindowsError:
-                    pass
+                    except WindowsError:
+                        pass
 
-                winreg.CloseKey( core_key )
+                    winreg.CloseKey( core_key )
 
             except WindowsError:
-                print( 'PythonCore not found in %s for %s' % (level, arch) )
+                print( '%s not found in %s for %s' % (vendor_key, level, arch) )
 
 if __name__ == '__main__':
     sys.exit( main( sys.argv ) )
